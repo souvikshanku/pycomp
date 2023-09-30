340cd7b0dda1e3ce832ce6c83125e42abf557c9d
Souvik Bhattacharyya <souvikshanku@gmail.com>
2023-09-30 23:08:24 +0530

    Decompression works!

--- huffman/huffman.py
+++ huffman/huffman.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
+import os
 
-from node import Node, tprint
+from node import Node
 
 
 def _build_tree(string: str) -> Node:
@@ -47,14 +48,15 @@ def _get_encoding(node) -> dict:
 
 
 def encode(string: str) -> bytearray:
+    header = str(len(string)) + "\n"
+
     root_node = _build_tree(string)
     encoding = _get_encoding(root_node)
 
-    header = ""
     for char in set(string):
-        header += char + " " + encoding[char] + "\n"
+        header += char + " " + encoding[char] + chr(27)  # Escape
 
-    header += chr(8)
+    header += chr(8)  # Backspace
 
     data = ""
     for char in string:
@@ -79,13 +81,72 @@ def compress(path: str):
     with open("./compressed.bin", "wb") as file:
         file.write(comp_data)
 
-    return comp_data
+    og = os.path.getsize(path)
+    new = os.path.getsize("./compressed.bin")
+    print("Compression Ratio Achieved:", og / new)
+    return
+
+
+def decompress(path: str):
+    with open(path, "rb") as file:
+        c_data = file.read()
+
+    length = ""
+
+    for i in range(len(c_data)):
+        if c_data[i] == 10:
+            break
+        length += chr(c_data[i])
+
+    length = int(length)
+
+    header = ""
+    for j, byte in enumerate(c_data[i + 1:], i + 1):
+        if byte == 8:
+            break
+        header += chr(byte)
+
+    encoding = {}
+    for line in header.split(chr(27))[:-1]:
+        encoding[line[2:]] = line[0]
+
+    binary_str = ""
+
+    for k in c_data[j + 1:]:
+        binary_str += "0" * (8 - len(bin(k)[2:])) + bin(k)[2:]
+
+    key = ""
+    decomp_data = ""
+
+    for char in binary_str:
+        if len(decomp_data) < length:
+            key += char
+
+            if key in encoding:
+                decomp_data += encoding[key]
+                key = ""
+
+    with open("./decompressed.txt", "wb") as file:
+        file.write(decomp_data.encode("ascii"))
+
+    return
 
 
 if __name__ == "__main__":
-    string = "aaaaaaaaasavemeeeeee"
-    root_node = _build_tree(string)
-    tprint(root_node)
-    print(_get_encoding(root_node))
-    print(list(encode(string)))
-    # compress("./sample.txt")
+    # from node import tprint
+    # string = "aaaaaaaaasavemeeeeee"
+    # root_node = _build_tree(string)
+    # tprint(root_node)
+    # print(_get_encoding(root_node))
+    # print(list(encode(string)))
+
+    compress("./sample.txt")
+    decompress("./compressed.bin")
+
+    with open("./sample.txt", "rb") as file:
+        og = file.read().decode("ascii")
+
+    with open("./decompressed.txt", "rb") as file:
+        new = file.read().decode("ascii")
+
+    assert og == new
