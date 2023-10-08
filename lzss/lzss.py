a28b3b0134d6a05a23a806bdab3a8d58a08e4df8
Souvik Bhattacharyya <souvikshanku@gmail.com>
2023-10-08 17:47:58 +0530

    Probably works now

--- lzss/lzss.py
+++ lzss/lzss.py
@@ -1,47 +1,45 @@
 with open("sample.txt", "rb") as file:
     data = file.read().decode("ascii")
 
-
 print(data)
-# print(len(data))
 
-BUFFER_SIZE = 32
+BUFFER_SIZE = 16
 SEARCH_BUFFER = ""
 final = ""
 
 
 def search(SEARCH_BUFFER, data, pos):
     found_at = -1
+    i = 1  # == number of consecutive char matches
 
-    while True:
-        if pos < len(data) and data[pos] in SEARCH_BUFFER:
-            found_at = SEARCH_BUFFER.find(data[pos])
-            # print(data[pos], "found_at - ", found_at)
-            pos += 1
-        else:
-            return found_at, pos
+    while i <= len(data) - pos and data[pos:pos+i] in SEARCH_BUFFER:
+        found_at = SEARCH_BUFFER.find(data[pos:pos+i])
+        # print(data[pos:pos+i], "found_at - ", found_at)
+        i += 1
+
+    return found_at, i - 1
 
 
 pos = 0
 
 while pos < len(data):
-
-    # print(f"'{SEARCH_BUFFER}', pos = {pos}")
-    found_at, end = search(SEARCH_BUFFER, data, pos)
-    # print(f"<{found_at}, {end - pos}>")
+    found_at, num_matches = search(SEARCH_BUFFER, data, pos)
 
     if found_at != -1:
-        final += f"<{found_at}, {end - pos}>"
+        to_replace_with = f"<{found_at},{num_matches}>"
+
+        if len(to_replace_with) < num_matches:
+            final += f"<{found_at},{num_matches}>"
+        else:
+            final += SEARCH_BUFFER[found_at:found_at + num_matches]
     else:
         final += data[pos]
 
-    if end - pos > 1:
-        pos = end
+    if num_matches > 0:
+        pos = pos + num_matches
     else:
         pos += 1
 
     SEARCH_BUFFER = data[:pos][-BUFFER_SIZE:]
-
-    # print(f"'{SEARCH_BUFFER}'", "\n--------")
 
 print(final)
